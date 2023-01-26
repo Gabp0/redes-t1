90d4ab315ec951aa4cabf329f3661a1d9126fbb6
Gabp0 <gabriel.pontarolo045@gmail.com>
2023-01-26 17:14:56 -0300

    checksum 8 bits

--- src/checksum.cpp
+++ src/checksum.cpp
@@ -1,116 +1,40 @@
-#include <bits/stdc++.h>
-#include <string.h>
+#include "checksum.h"
 #include <iostream>
+
 using namespace std;
 
-string ones_complement(string data)
+char checksum8(char *data, unsigned int size)
 {
-    for (size_t i = 0; i < data.length(); i++)
-    {
-        if (data[i] == '0')
-            data[i] = '1';
-        else
-            data[i] = '0';
-    }
+  uint8_t sum = 0;
+  
+  for (size_t i = 0; i < size; i++) {
+    sum += (uint8_t) data[i];
+  }
+  
+  sum = ~sum;
 
-    return data;
+  cout << sum << endl;
+  
+  return sum;
 }
 
-string calcCheckSum(string data, int block_size = 8)
-{
-    int n = data.length();
-    if (n % block_size != 0)
-    {
-        int pad_size = block_size - (n % block_size);
-        for (int i = 0; i < pad_size; i++)
-        {
-            data = '0' + data;
-        }
-    }
+//char[2] checksum16(char *data, unsigned int size)
+//{
+  //uint16_t sum = 0;
 
-    string result = "";
 
-    for (int i = 0; i < block_size; i++)
-    {
-        result += data[i];
-    }
+  //for (size_t i = 0; i < size; i+=2) {
+    //uint16_t cbyte = data[i];
+    //cbyte = (cybte << 8);
+    //cbyte = (cybte | data[i + 1])
 
-    for (int i = block_size; i < n; i += block_size)
-    {
-        string next_block = "";
+    //sum += (uint16_t) data[i];
 
-        for (int j = i; j < i + block_size; j++)
-        {
-            next_block += data[j];
-        }
+  //}
+  
+  //sum = ~sum;
 
-        string additions = "";
-        int sum = 0, carry = 0;
-
-        for (int k = block_size - 1; k >= 0; k--)
-        {
-            sum += (next_block[k] - '0') + (result[k] - '0');
-            carry = sum / 2;
-            if (sum == 0)
-            {
-                additions = '0' + additions;
-                sum = carry;
-            }
-            else if (sum == 1)
-            {
-                additions = '1' + additions;
-                sum = carry;
-            }
-            else if (sum == 2)
-            {
-                additions = '0' + additions;
-                sum = carry;
-            }
-            else
-            {
-                additions = '1' + additions;
-                sum = carry;
-            }
-        }
-
-        string final = "";
-
-        if (carry == 1)
-        {
-            for (int l = additions.length() - 1; l >= 0;
-                 l--)
-            {
-                if (carry == 0)
-                {
-                    final = additions[l] + final;
-                }
-                else if (((additions[l] - '0') + carry) % 2 == 0)
-                {
-                    final = "0" + final;
-                    carry = 1;
-                }
-                else
-                {
-                    final = "1" + final;
-                    carry = 0;
-                }
-            }
-
-            result = final;
-        }
-        else
-            result = additions;
-    }
-    return ones_complement(result);
-}
-
-bool checker(string rec_message, string rec_checksum, int block_size = 8)
-{
-    string msg_checksum = calcCheckSum(rec_message, block_size);
-
-    if (count(msg_checksum.begin(), rec_checksum.end(), '0') == block_size)
-        return true;
-    else
-        return false;
-}
-
+  //cout << sum << endl;
+  
+  //return sum;
+//}
