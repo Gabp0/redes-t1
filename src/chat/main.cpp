112b80f736e6e9edb0aeb2b372f4548b887df48b
gab <gabriel.pontarolo045@gmail.com>
2023-02-23 02:39:04 -0300

    main novo?

--- src/chat/main.cpp
+++ src/chat/main.cpp
@@ -1,22 +1,10 @@
 #include "chat.h"
-#include <thread>
-
-void receive(Chat *cs)
-{
-    cs->receive();
-}
 
 int main(int argc, char const *argv[])
 {
     Chat cs("lo1", "lo2");
 
-    std::thread th1(receive, &cs);
-
-    while (cs.readFromUser())
-    {
-    }
-
-    th1.join();
+    cs.loadChat();
 
     return 0;
 }
